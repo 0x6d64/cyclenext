06ecb695fe36d6ccca962c0292e75d4e440f6be8
Martin Demling <0x6d64@martin-demling.de>
2021-03-14 11:15:13 +0200

    basic refactoring
    - conform to PEP8 naming
    - separate intomethods
    - move creation of config into method
    - reduce file access
    - add docstrings, improve naming

--- cyclenext.py
+++ cyclenext.py
@@ -6,90 +6,121 @@ import platform
 from subprocess import call
 import sys
 
-# TODO: test on Windows
-# TODO: calculate limit dynamically
-# TODO: detect TW directory by looking at .taskrc config file
-
-taskdir = r"/home/martin/.task"
-taskcommand = "/usr/bin/task"
-filesToWatch = ["undo.data", "backlog.data"]  # this should be enough to get all operations including sync of remote tasks
-getLazyAfterSecs = 3 * 60
-forceRedrawSecs_default = 10  # this also determines the resolution of "last change x ago" info
-loopDelaySecs_default = 0.33
+config = dict()
 
 
-def getFileAgeSecs(filename):
+def create_config_dict(config_to_modify):
+    config_to_modify.update(
+        {
+            "taskdir": os.path.join(os.path.expanduser("~/.task")),
+            "taskcommand": "/usr/bin/task",
+            "get_lazy_after_secs": 3 * 60,
+            "force_redraw_secs_default": 10,
+            "loop_delay_secs_default": 0.33,
+            "platform_is_windows": platform.system() == "Windows"
+        }
+    )
+
+
+def redraw(limit, filterstring, seconds_since_change):
+    """draw a new task list"""
+    limitstring = "limit:{limit}".format(limit=limit)
+    minutes_ago = seconds_since_change / 60
+
+    if minutes_ago < 60:
+        last_changed_message = "last change %1.0f minutes ago" % minutes_ago
+    elif minutes_ago < (24 * 60):
+        last_changed_message = "last change %1.1f hours ago" % (minutes_ago / 60)
+    else:
+        last_changed_message = "last change %1.1f days ago" % (minutes_ago / (24 * 60))
+    clear_terminal()
+    print(last_changed_message)
+    call([config.get("taskcommand"), filterstring, "rc.gc=off", limitstring])
+
+
+def get_file_age_secs(filename):
+    """get file age for one file"""
     t_mod = os.path.getmtime(filename)
-    secondsSince = time.time() - t_mod
-    return secondsSince
+    seconds_since = time.time() - t_mod
+    return seconds_since
 
-def getMinimalAgeSecs(pathList):
-    ages = []
-    for filename in pathList:
-        ages.append(getFileAgeSecs(filename))
+
+def get_minimal_age_secs(path_list):
+    """get minimal age of list of files"""
+    ages = [get_file_age_secs(filename) for filename in path_list]
     return min(ages)
 
-def redraw(limit, filterstring):
-    
-    limitstring = "limit:" + str(limit)
-    minutesAgo = getMinimalAgeSecs(pathList) / 60
-    
-    clearTerminal()
-    if minutesAgo < 60:
-        print("last change %1.0f minutes ago" % (minutesAgo))
-    elif minutesAgo < (24 * 60):
-        print("last change %1.1f hours ago" % (minutesAgo / 60))
-    else:
-        print("last change %1.1f days ago" % (minutesAgo / (24 * 60)))
-    call([taskcommand, filterstring, "rc.gc=off", limitstring])
-    pass
 
-def clearTerminal():
-    os.system('cls' if platformIsWindows else "clear")
-    
-def calcLimit(termsize):
-    tlines = int(termsize[0])
-    tcols = int(termsize[1])
-    if tcols > 90:
+def clear_terminal():
+    clear_command = 'cls' if config.get("platform_is_windows") else "clear"
+    os.system(clear_command)
+
+
+def calc_terminal_size_limit(termsize):
+    """
+    calculates the number of lines to display based on available lines and columns in terminal
+    a narrow terminal can support only a few lines due to the description being wrapped
+    all values here are determined by experience
+
+    :param termsize:
+    :return:
+    """
+    terminal_lines = int(termsize[0])
+    terminal_columns = int(termsize[1])
+    if terminal_columns > 90:
         factor = 0.6
-    elif tcols > 65:
+    elif terminal_columns > 65:
         factor = 0.4
     else:
         factor = 0.2
-    limit = int(tlines * factor)
-    return (limit if limit > 0 else 1)
+    limit = int(terminal_lines * factor)
+    return limit if limit > 0 else 1
 
-# prepare
-platformIsWindows = (platform.system() == "Windows")
-pathList = []
-for filename in filesToWatch:
-    pathToAppend = os.path.join(taskdir, filename)
-    if os.path.isfile(pathToAppend):
-        pathList.append(pathToAppend)
-timeSinceRedrawSecs = 0.0
-termsizeOld = (0, 0)
-filter = sys.argv[1] if len(sys.argv) > 1 else 'ready' # use ready as default filter
 
-try:
-    while True:
-        termsize = os.popen('stty size', 'r').read().split()
-        fileAgeSecs = getMinimalAgeSecs(pathList)
-        lazyFactor = 5 if (fileAgeSecs > getLazyAfterSecs) else 1
-        forceRedrawSecs = forceRedrawSecs_default * lazyFactor
-        loopDelaySecs = loopDelaySecs_default * lazyFactor
-        
-        termSizeChanged = (termsize != termsizeOld)
-        forceRedraw = (timeSinceRedrawSecs > forceRedrawSecs)
-        fileChanged = (fileAgeSecs < (loopDelaySecs * 1.8))
-            
-        if termSizeChanged or forceRedraw or fileChanged:
-            redraw(calcLimit(termsize), filter)
-#             print('\a') # terminal bell for debug
-            timeSinceRedrawSecs = 0
-            time.sleep(loopDelaySecs)
-            
-        time.sleep(loopDelaySecs)
-        timeSinceRedrawSecs += loopDelaySecs
-        termsizeOld = termsize
-except KeyboardInterrupt:
-    print("   bye!")
+def get_relevant_paths():
+    filenames_to_watch = [  # these should be enough to get all operations including sync of remote tasks
+        "undo.data",
+        "backlog.data"
+    ]
+    taskdir = config.get("taskdir")
+    relevant_paths_candidates = [os.path.join(taskdir, item) for item in filenames_to_watch]
+    relevant_paths_existing = [item for item in relevant_paths_candidates if os.path.isfile(item)]
+    return relevant_paths_existing
+
+
+def run_main():
+    create_config_dict(config)
+
+    path_list = get_relevant_paths()
+    time_since_redraw = 0.0
+    terminal_size_old = (0, 0)
+    taskw_filter_string = sys.argv[1] if len(sys.argv) > 1 else 'ready'  # use ready as default filter
+
+    try:
+        while True:
+            terminal_size = os.popen('stty size', 'r').read().split()
+            file_age_seconds = get_minimal_age_secs(path_list)
+            lazy_factor = 5 if (file_age_seconds > config.get("get_lazy_after_secs")) else 1
+            force_redraw_secs = config.get("force_redraw_secs_default") * lazy_factor
+            loop_delay_seconds = config.get("loop_delay_secs_default") * lazy_factor
+
+            terminal_size_did_change = (terminal_size != terminal_size_old)
+            force_redraw = (time_since_redraw > force_redraw_secs)
+            file_changed = (file_age_seconds < (loop_delay_seconds * 1.8))
+
+            if terminal_size_did_change or force_redraw or file_changed:
+                redraw(calc_terminal_size_limit(terminal_size), taskw_filter_string, file_age_seconds)
+                #             print('\a') # terminal bell for debug
+                time_since_redraw = 0
+                time.sleep(loop_delay_seconds)
+
+            time.sleep(loop_delay_seconds)
+            time_since_redraw += loop_delay_seconds
+            terminal_size_old = terminal_size
+    except KeyboardInterrupt:
+        print("   bye!")
+    pass
+
+
+if __name__ == '__main__':
+    run_main()
